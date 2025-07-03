7dc3083a234dba12ffc88835635f4b153b19ed09
Long Do <Longdt57@gmail.com>
2025-07-04 00:15:31 +0700

    Fix tests

---
+++ GitUserTests/Sources/SampleTests.swift
@@ -0,0 +1,16 @@
+//
+//  SampleTests.swift
+//  GitUser
+//
+//  Created by Long Do on 4/7/25.
+//
+
+import XCTest
+@testable import GitUser
+
+final class SampleTests: XCTestCase {
+    
+    func testAddition() {
+        XCTAssertEqual(2+3, 5, "2 + 3 should equal 5")
+    }
+}
